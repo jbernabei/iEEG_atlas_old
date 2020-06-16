c03979fa1a606816c32979fe27ed186891ba3748
John Bernabei <jbernabei@Johns-MacBook-Pro-5.local>
2020-06-15 22:04:29 -0600

    starting repository

---
+++ analysis/create_project_figures.m
@@ -0,0 +1,24 @@
+%% create_project_figures.m
+% John Bernabei
+% With assistance from Ian Ong
+% Litt Laboratory
+
+%% Set up workspace
+
+%% Figure 1A: visualize adjacency matrix (one panel in pipeline figure)
+
+%% Figure 1B: visualize brain (one panel in pipeline figure)
+
+%% Figure 2A: anatomical analysis
+
+% run all patients in atlas
+
+%% Figure 2B: cross - validate out-of-bag predictions
+
+%% Figure 2C: multi-linear model
+
+%% Figure 3: Distinguish between epilepsy type
+
+%% Figure 4A: localize in good outcome patients
+
+%% Figure 4B: localize in poor outcome patients
